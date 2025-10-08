8416cd9b964ec98e9be2cc5e48ccf625e75360e0
arpi2003ta <145153525+arpi2003ta@users.noreply.github.com>
2025-10-09 01:21:49 +0530

    Delete server directory

--- server/utils/socket.js
+++
@@ -1,8 +0,0 @@
-export const emitSocketEvent = (req, userId, event, data) => {
-  const io = req.app.get("io");
-  const socketId = io.sockets.sockets.get(userId);
-
-  if (socketId) {
-    io.to(socketId).emit(event, data);
-  }
-};
